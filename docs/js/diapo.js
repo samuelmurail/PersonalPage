01f2886177d688a812340fa2b5d3ff363525b151
Samuel Murail <samuel.murail@univ-paris-diderot.fr>
2019-07-18 23:49:20 +0200

    Add automatic slideshow

--- docs/js/diapo.js
+++ docs/js/diapo.js
@@ -1,5 +1,5 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 0;
+//showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
@@ -14,15 +14,23 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  //var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
+  //for (i = 0; i < dots.length; i++) {
+  //    dots[i].className = dots[i].className.replace(" active", "");
+  //}
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  //dots[slideIndex-1].className += " active";
 } 
+
+function showSlides_auto() {
+    slideIndex++;
+    showSlides(slideIndex)
+    setTimeout(showSlides_auto, 6000); // Change image every 2 seconds
+} 
+
+showSlides_auto()
